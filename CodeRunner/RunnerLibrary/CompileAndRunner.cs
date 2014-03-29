2129b7854a95c0f803903c69dbec8e528f8748ee
TichShowers <tichsc2@gmail.com>
2014-03-29 14:19:09 +0100

    Added missing files

---
+++ CodeRunner/RunnerLibrary/CompileAndRunner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace RunnerLibrary
+{
+    public class CompileAndRunner : Runner, IRunner
+    {
+        public IRunner Runner { get; set; }
+        public string CompiledRunnableFullPath { get; set; }
+        public new string Input { set { Runner.Input = value; } }
+        public new string Output
+        {
+            get { return Runner.Output; }
+        }
+        public new string Error
+        {
+            get { return Runner.Error; }
+        }
+
+        public override void Run()
+        {
+            InsertRunnableIntoArguments();
+            base.Run();
+            Runner.RunFileFullPath = CompiledRunnableFullPath;
+            Runner.Run();
+        }
+
+        private void InsertRunnableIntoArguments()
+        {
+            CommandLineArguments = String.Format(CommandLineArguments, CompiledRunnableFullPath);
+        }
+    }
+}
